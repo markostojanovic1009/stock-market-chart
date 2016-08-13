e7f117bcd6acb8c6b1cf2a8ebc542e3cd3322270
Marko Stojanovic <markostojanovic1009@protonmail.com>
2016-08-13 21:32:57 +0200

    Add stock_values table to migration files

--- migrations/development/20160813124911_inital.js
+++ migrations/development/20160813124911_inital.js
@@ -4,12 +4,17 @@ exports.up = function(knex, Promise) {
             stocks.increments();
             stocks.string('symbol').notNullable().unique();
             stocks.timestamp('created_at').defaultTo(knex.fn.now());
+        }).createTable('stock_values', (table) => {
+            table.increments();
+            table.integer('stock_id').references('stocks.id').onDelete('CASCADE');
+            table.decimal('value', 14, 2);
+            table.date('day');
         })
     ])
 };
 
 exports.down = function(knex, Promise) {
     return Promise.all([
-        knex.schema.dropTableIfExists('stocks')
+        knex.schema.dropTableIfExists('stock_values').dropTableIfExists('stocks')
     ]);
 };
--- migrations/test/20160813130313_initial.js
+++ migrations/test/20160813130313_initial.js
@@ -3,13 +3,18 @@ exports.up = function(knex, Promise) {
         knex.schema.createTable('stocks', (stocks) => {
             stocks.increments();
             stocks.string('symbol').notNullable().unique();
-            stocks.date('created_at').defaultTo(knex.fn.now());
+            stocks.timestamp('created_at').defaultTo(knex.fn.now());
+        }).createTable('stock_values', (table) => {
+            table.increments();
+            table.integer('stock_id').references('stocks.id').onDelete('CASCADE');
+            table.decimal('value', 14, 2);
+            table.date('day');
         })
     ])
 };
 
 exports.down = function(knex, Promise) {
     return Promise.all([
-        knex.schema.dropTableIfExists('stocks')
+        knex.schema.dropTableIfExists('stock_values').dropTableIfExists('stocks')
     ]);
 };
