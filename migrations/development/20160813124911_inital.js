138830d6a3d910eada098a9d8f8e1c90e156e11d
Marko Stojanovic <markostojanovic1009@protonmail.com>
2016-08-13 20:44:43 +0200

    Add migrations and seeds

---
+++ migrations/development/20160813124911_inital.js
@@ -0,0 +1,15 @@
+exports.up = function(knex, Promise) {
+    return Promise.all([
+        knex.schema.createTable('stocks', (stocks) => {
+            stocks.increments();
+            stocks.string('symbol').notNullable().unique();
+            stocks.timestamp('created_at').defaultTo(knex.fn.now());
+        })
+    ])
+};
+
+exports.down = function(knex, Promise) {
+    return Promise.all([
+        knex.schema.dropTableIfExists('stocks')
+    ]);
+};
---
+++ seeds/test/initial_seed.js
@@ -0,0 +1,8 @@
+
+exports.seed = function(knex, Promise) {
+  return Promise.join(
+    // Deletes ALL existing entries
+
+    // Inserts seed entries
+  );
+};
